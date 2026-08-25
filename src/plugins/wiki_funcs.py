95340291f653ab935fb394527f6045754d9f0ff2
Henrique Aroeira <henrique.aroeira43@gmail.com>
2026-08-25 12:03:31 -0300

    fixed position of logging info, as it was causing issues

--- src/plugins/wiki_funcs.py
+++ src/plugins/wiki_funcs.py
@@ -165,8 +165,8 @@ class WikiCommands(commands.Cog):
         cfg = _WIKIS[source]
         if msg.author.bot or not (matches:=match_all(pattern, msg.content)):
             return
-        logger.info(f"User {msg.author} searched for {m} in {source} wiki.")
         for m in matches[:10]:
+            logger.info(f"User {msg.author} searched for {m} in {source} wiki.")
             if ret := await search_wiki_titles(m,limit = 5, searchlink_internal=cfg[2]):
                 url = f"{cfg[0]}{ret[0].replace(' ', '_')}"
                 if embed := await create_embed_from_wiki(ret[0], url, source):
